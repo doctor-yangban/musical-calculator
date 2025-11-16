d8d672968738e176eddab0dd73b9782d1d37e74b
chonggi-tokhu <213584637+doctor-yangban@users.noreply.github.com>
2025-11-16 16:30:37 +0900

    .

--- frontend/components/calculator.vue
+++ frontend/components/calculator.vue
@@ -9,14 +9,15 @@ import divinput from './divinput.vue';
                 <div class="display">
 
                     <divinput
-                        :style="{ 'font-size': '.6rem', 'max-width': '50vw', 'min-width': 'calc(var(--min-width) / 2)', 'position': 'relative' }"
+                        :style="{ 'font-size': 'calc(.6rem * var(--calculatorSize))', 'max-width': '50vw', 'min-width': 'calc(var(--min-width) / 2)', 'position': 'relative' }"
                         :model="currExprStr" />
-                    <div style="font-size:.6rem;font-weight: bold;font-style:italic;max-width:50vw;">
+                    <div
+                        style="font-size:calc(.6rem * var(--calculatorSize));font-weight: bold;font-style:italic;max-width:50vw;">
                         {{ currAns }}</div>
 
                 </div>
             </div>
-            <div style="display: flex;">
+            <div style="display: flex;margin-top: calc(-.5rem * var(--calculatorSize));">
                 <div class="calc-button" style="height: 2em;"><span
                         style="background: lightgrey;color:#222222;height: 2.4em;display: block;width: 2rem;">O</span>
                 </div>
@@ -94,51 +95,63 @@ import divinput from './divinput.vue';
     </div>
 </template>
 <style scoped>
+* {
+    --calculatorSize: 1;
+}
+
+@media (max-width:850px) {
+    * {
+        --calculatorSize: calc(100vw / 400px) !important;
+    }
+}
+
 .musical-calculator {
-    height: 14em;
+    height: calc(14em * var(--calculatorSize));
     background: silver;
     width: fit-content;
-    padding: 0.5rem;
+    padding: calc(0.5rem * var(--calculatorSize));
     border-radius: 6%;
     box-shadow: 2px 2px 1px 1px #000000, -2px -2px 1px 1px #000000, 2px -2px 1px 1px #000000, -2px 2px 1px 1px #000000;
 }
 
 .musical-calculator tr td {
-    margin: 0.5rem;
+    margin-top: calc(0.5rem * var(--calculatorSize));
     border-radius: 8vmax;
 }
 
 .musical-calculator>div>div>div {
-    height: 1em;
-    margin: 0.1rem;
-    margin-top: 0.5em;
-    min-width: 2rem;
+    height: calc(1em * var(--calculatorSize));
+    margin: calc(0.1rem * var(--calculatorSize));
+    margin-top: calc(0.5em * var(--calculatorSize));
+    min-width: calc(2rem * var(--calculatorSize));
 }
 
 .musical-calculator>div>div>div>span {
     border: 1px solid #000000;
-    border-radius: 8vmax;
-    min-width: 2rem;
+    border-radius: calc(8vmax * var(--calculatorSize));
+    min-width: calc(2rem * var(--calculatorSize));
     display: block;
-    margin-top: 0.5em;
+    margin-top: calc(0.5em * var(--calculatorSize));
     text-align: center;
+    font-size: calc(100% * var(--calculatorSize));
+    background: transparent;
 }
 
 .display {
     background: #1c873c;
     color: #000000;
-    padding-top: .1rem;
-    padding-bottom: .1rem;
-    height: 1.6rem;
-    margin-left: 0.5rem;
-    margin-right: 0.5rem;
-    font-size: 1.2rem;
+    padding-top: calc(.1rem * var(--calculatorSize));
+    padding-bottom: calc(.1rem * var(--calculatorSize));
+    height: calc(1.2rem * var(--calculatorSize));
+    margin-left: calc(.5rem * var(--calculatorSize));
+    margin-right: calc(.5rem * var(--calculatorSize));
+    font-size: calc(1.2rem * var(--calculatorSize));
     font-weight: bold;
     font-style: italic;
-    line-height: .7rem;
+    line-height: calc(.6rem * var(--calculatorSize));
     overflow: scroll;
     max-width: 100vw;
-    --min-width: 22.5rem;
+    --min-width: calc(22.5rem * var(--calculatorSize));
     min-width: var(--min-width);
 }
 
